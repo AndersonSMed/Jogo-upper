7ed6ad26686666a9983fdae243c38c64b46ed70a
Anderson Santos de Medeiros <anderson.santos.medeiros@hotmail.com>
2019-01-27 17:36:19 -0300

    Basic GameManager added, and started to create a HUD in our game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    // Instance created to use a singleton pattern
+    private static GameManager instance;
+
+    [SerializeField]
+    // Stores the initial life of our player
+    private float initialLife = 10f;
+
+    // Stores if the game is paused
+    private bool gamePaused = false;
+    // Stores the actual life of our player
+    private float actualLife;
+
+    // Getter of instance
+    public static GameManager Instance {
+        get {
+            return instance;
+        }
+    }
+
+    // When this object is created, call this method and create a instance, if there's none
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+        }else if(instance != this) {
+            Destroy(instance);
+        }
+        // This method is called for Unity to know that instance can't be destroyed when we change our scene
+        DontDestroyOnLoad(instance);
+    }
+
+    // Method called when we need to pause our game
+    public void PauseGame() {
+        gamePaused = true;
+    }
+
+    // Method called when our game is paused and we need to resume it
+    public void ResumeGame() {
+        gamePaused = false;
+    }
+
+    // Return if our game is paused or not
+    public bool IsGamePaused() {
+        return gamePaused;
+    }
+
+    // Method called when we need to damage our player
+    public void DamagePlayer(float damage) {
+        actualLife -= damage;
+    }
+
+    // Returns the actual life of our player
+    public float GetLife() {
+        return actualLife;
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -40,73 +40,82 @@ public class Player : MonoBehaviour {
 
     // This function is called once per frame
     private void Update () {
-        // Inputs are used to create multiple forms to control the game
-        // Get if the button for jump was pressed and store that information
-        // The input values range between [0,1], for this case
-        if (Input.GetAxis("Jump") != 0f) {
-            jumping = true;
-        } else {
-            jumping = false;
-        }
-        // Here the input range between [-1,1], -1 means that the player is pressing left and 1 means right
-        // 0 means that the player is not walking
-        // Stores that info in our variable horizontalMovement
-        horizontalMovement = Input.GetAxis("Horizontal");
-        if (horizontalMovement != 0f) {
-            // Stores if the player is walking
-            walking = true;
-            // Verifies if the player is walking left
-            if (horizontalMovement < 0f) {
-                // Then check if the rotation in the Y axis is different from 180
-                if (transform.rotation.y != 180f)
-                    // If it's different, rotate the player
-                    transform.rotation = new Quaternion(0f, 180f, 0f, transform.rotation.w);
+        // Verify if the game is paused
+        if (!GameManager.Instance.IsGamePaused()) {
+            // Inputs are used to create multiple forms to control the game
+            // Get if the button for jump was pressed and store that information
+            // The input values range between [0,1], for this case
+            if (Input.GetAxis("Jump") != 0f) {
+                jumping = true;
             } else {
-                // Do the same thing here, but in this case we check if the player is walking to the right
-                // And if the rotation is equals to 0
-                if (transform.rotation.y != 0f)
-                    // If it's different, rotate the player
-                    transform.rotation = new Quaternion(0f, 0f, 0f, transform.rotation.w);
+                jumping = false;
             }
-        } else {
-            // Otherwise the player is not walking, so stores that to our variable
-            walking = false;
-        }
-        // If the player's not pressing the jump button and he's on the ground, then the player isn't in the air
-        if(onGround && !jumping) {
-            onAir = false;
-        }
-        // If press the button E, then release the fireball
-        if (Input.GetKeyUp(KeyCode.E)) {
-            // Instantiate a new fireball
-            GameObject ball = Instantiate(fireBall);
-            // Set it's position to our spawn
-            ball.transform.position = new Vector2(fireBallSpawn.transform.position.x, fireBallSpawn.transform.position.y);
-            // If the player is facing right, then call the FireRight method from the fireball script
-            if (transform.rotation.y == 0f)
-                ball.GetComponent<Fireball>().FireRight();
-            // Otherwise, call the method FireLeft from the fireball script
-            else
-                ball.GetComponent<Fireball>().FireLeft();
+            // Here the input range between [-1,1], -1 means that the player is pressing left and 1 means right
+            // 0 means that the player is not walking
+            // Stores that info in our variable horizontalMovement
+            horizontalMovement = Input.GetAxis("Horizontal");
+            if (horizontalMovement != 0f) {
+                // Stores if the player is walking
+                walking = true;
+                // Verifies if the player is walking left
+                if (horizontalMovement < 0f) {
+                    // Then check if the rotation in the Y axis is different from 180
+                    if (transform.rotation.y != 180f)
+                        // If it's different, rotate the player
+                        transform.rotation = new Quaternion(0f, 180f, 0f, transform.rotation.w);
+                } else {
+                    // Do the same thing here, but in this case we check if the player is walking to the right
+                    // And if the rotation is equals to 0
+                    if (transform.rotation.y != 0f)
+                        // If it's different, rotate the player
+                        transform.rotation = new Quaternion(0f, 0f, 0f, transform.rotation.w);
+                }
+            } else {
+                // Otherwise the player is not walking, so stores that to our variable
+                walking = false;
+            }
+            // If the player's not pressing the jump button and he's on the ground, then the player isn't in the air
+            if (onGround && !jumping) {
+                onAir = false;
+            }
+            // If press the button E, then release the fireball
+            if (Input.GetKeyUp(KeyCode.E)) {
+                // Instantiate a new fireball
+                GameObject ball = Instantiate(fireBall);
+                // Set it's position to our spawn
+                ball.transform.position = new Vector2(fireBallSpawn.transform.position.x, fireBallSpawn.transform.position.y);
+                // If the player is facing right, then call the FireRight method from the fireball script
+                if (transform.rotation.y == 0f)
+                    ball.GetComponent<Fireball>().FireRight();
+                // Otherwise, call the method FireLeft from the fireball script
+                else
+                    ball.GetComponent<Fireball>().FireLeft();
+            }
         }
     }
 
     // This function is called a fixed number of times per frames
     private void FixedUpdate() {
-        // Here we verify if the player is walking and his velocity in the X axis is less than our max speed
-        if (walking && rb.velocity.x < maxSpeed) {
-            // If that is true, than add some force to our player, to create movement
-            rb.AddForce(Vector2.right * horizontalMovement * walkingAcceleration);
-        }else if (!walking) {
-            // If the player is not walking, then reset his velocity on the axis X
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-        // If the player wants to jump, and his isn't on air
-        if (jumping && !onAir) {
-            // Apply some Impulse in the axis Y
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            // And now his is on air
-            onAir = true;
+        // Verify if the game is paused
+        if (!GameManager.Instance.IsGamePaused()) {
+            // Here we verify if the player is walking and his velocity in the X axis is less than our max speed
+            if (walking && rb.velocity.x < maxSpeed) {
+                // If that is true, than add some force to our player, to create movement
+                rb.AddForce(Vector2.right * horizontalMovement * walkingAcceleration);
+            }
+            else if (!walking) {
+                // If the player isn't already stopped
+                if (rb.velocity.x != 0)
+                    // If the player is not walking, then reset his velocity on the axis X
+                    rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+            // If the player wants to jump, and his isn't on air
+            if (jumping && !onAir) {
+                // Apply some Impulse in the axis Y
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                // And now his is on air
+                onAir = true;
+            }
         }
     }
 
